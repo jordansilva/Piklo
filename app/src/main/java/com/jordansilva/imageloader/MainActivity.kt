6ba1451d2ccf574fc0ddcf467d265cef737203e8
Jordan Silva <silva.jordan@gmail.com>
2019-11-01 18:34:20 +0100

    Initial commit

---
+++ app/src/main/java/com/jordansilva/imageloader/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jordansilva.imageloader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jordansilva.imageloader.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
